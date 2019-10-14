9938751c789a32e83c89e78e9dbab5755abff9c4
Ashley Wegwerth <ashley.wegwerth@gmail.com>
2019-10-14 18:00:24 -0500

    time blocks change color based on current time

--- day_planner/script.js
+++ day_planner/script.js
@@ -1,22 +1,137 @@
 $(document).ready(function() {
 
-// var task = $("textarea");
-var d = new Date();
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+//Global variables//
+var currentHour = moment().hour();
+console.log(currentHour);
+
+
 
 
 //Current Date//
-var newDate = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+var newDate = moment().format('LL');
 console.log(newDate);
 $("#currentDay").html(newDate);
 
 
-$("body").on("click", "button", function(event) {
-    event.preventDefault();
 
-});
+//Saving tasks by clicking the save button//
+// $("body").on("click", "button", function(event) {
+//     event.preventDefault();
+
+//     var task = $("textarea").val();
+//     localStorage.setItem("task", JSON.stringify(task));
+    
+//     setTask();
+// });
+
+// function setTask() {
+//     var task = JSON.parse(localStorage.getItem("task"));
+//     $("textarea").html(task);
+  
+// }
 
 
+
+//Time slots changing color based on current time//
+if (9 === currentHour) {
+      $(".9").attr("class", "present");
+  }
+else if (9 < currentHour) {
+      $(".9").attr("class", "past");
+  }
+else {
+      $(".9").attr("class", "future");
+  }
+
+
+
+if (10 === currentHour) {
+    $(".10").attr("class", "present");
+}
+else if (10 < currentHour) {
+    $(".10").attr("class", "past");
+}
+else {
+    $(".10").attr("class", "future");
+}
+
+
+
+if (11 === currentHour) {
+    $(".11").attr("class", "present");
+}
+else if (11 < currentHour) {
+    $(".11").attr("class", "past");
+}
+else {
+    $(".11").attr("class", "future");
+}
+
+
+if (12 === currentHour) {
+    $(".12").attr("class", "present");
+}
+else if (12 < currentHour) {
+    $(".12").attr("class", "past");
+}
+else {
+    $(".12").attr("class", "future");
+}
+
+
+if (13 === currentHour) {
+    $(".13").attr("class", "present");
+}
+else if (13 < currentHour) {
+    $(".13").attr("class", "past");
+}
+else {
+    $(".13").attr("class", "future");
+}
+
+
+if (14 === currentHour) {
+    $(".14").attr("class", "present");
+}
+else if (14 < currentHour) {
+    $(".14").attr("class", "past");
+}
+else {
+    $(".14").attr("class", "future");
+}
+
+
+if (15 === currentHour) {
+    $(".15").attr("class", "present");
+}
+else if (15 < currentHour) {
+    $(".15").attr("class", "past");
+}
+else {
+    $(".15").attr("class", "future");
+}
+
+
+if (16 === currentHour) {
+    $(".16").attr("class", "present");
+}
+else if (16 < currentHour) {
+    $(".16").attr("class", "past");
+}
+else {
+    $(".16").attr("class", "future");
+}
+
+
+if (17 === currentHour) {
+    $(".17").attr("class", "present");
+}
+else if (17 < currentHour) {
+    $(".17").attr("class", "past");
+}
+else {
+    $(".17").attr("class", "future");
+}
 
 });
 
